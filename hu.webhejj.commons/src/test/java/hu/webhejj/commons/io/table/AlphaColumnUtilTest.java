aaf2b5a6e41e2a4fea66a97c051240abde65e2f1
Gergely Nagy <greg@webhejj.hu>
2014-02-25 12:52:04 +0100

    Add copyright header comment to AlphaColumnUtilTest

--- hu.webhejj.commons/src/test/java/hu/webhejj/commons/io/table/AlphaColumnUtilTest.java
+++ hu.webhejj.commons/src/test/java/hu/webhejj/commons/io/table/AlphaColumnUtilTest.java
@@ -1,3 +1,11 @@
+/*
+ *  Copyright Gergely Nagy <greg@webhejj.hu>
+ *
+ *  Licensed under the Apache License, Version 2.0;
+ *  you may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ */
 package hu.webhejj.commons.io.table;
 
 import junit.framework.Assert;
